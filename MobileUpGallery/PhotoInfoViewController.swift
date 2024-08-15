2a1d24d09366bf20454b4d7e28d1fefd65f8d7eb
Vasilii Tikhonov <tihonovvasilij154@gmail.com>
2024-08-15 16:08:08 +0300

    added saving photos to the gallery

--- MobileUpGallery/PhotoInfoViewController.swift
+++ MobileUpGallery/PhotoInfoViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import SDWebImage
+import Photos
+
 
 class PhotoInfoViewController: UIViewController {
     
@@ -32,7 +34,6 @@ class PhotoInfoViewController: UIViewController {
     }()
     
     private lazy var exitBarButtonItem: UIBarButtonItem = {
-
         let barButtonItem = UIBarButtonItem(image: UIImage(systemName: "chevron.left"),
                                             style: .plain,
                                             target: self,
@@ -41,16 +42,13 @@ class PhotoInfoViewController: UIViewController {
         return barButtonItem
     }()
     
-    
     let photoImageView: UIImageView = {
         let image = UIImageView()
         image.contentMode = .scaleAspectFill
         image.clipsToBounds = true
         image.translatesAutoresizingMaskIntoConstraints = false
-        
         return image
     }()
-   
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -58,59 +56,36 @@ class PhotoInfoViewController: UIViewController {
         setupNavigationBar()
         configure(with: urlString)
         setupTitle()
-        
     }
     
     private func setupTitle() {
         let date = Date(timeIntervalSince1970: TimeInterval(photoDate))
         let dateFormatter = DateFormatter()
-        
-     
         dateFormatter.locale = Locale(identifier: "ru_RU")
-        
-
         dateFormatter.dateFormat = "d MMMM yyyy"
-        
         title = dateFormatter.string(from: date)
     }
-
     
     @objc func actionBarButtonTapped(sender: UIBarButtonItem) {
         guard let image = photoImageView.image else {
-            print("No image found")
             return
         }
-
+        
         let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
         activityViewController.popoverPresentationController?.barButtonItem = sender
-
-        activityViewController.completionWithItemsHandler = { activityType, completed, returnedItems, activityError in
-            if completed && activityType == .saveToCameraRoll {
-                self.saveImageToGallery(image: image)
+        
+        activityViewController.completionWithItemsHandler = { activity, success, items, error in
+            if success, let activityType = activity, activityType == .saveToCameraRoll {
+                let alert = UIAlertController(title: "Success",
+                                              message: "Photo saved to gallery!",
+                                              preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
             }
         }
-
+        
         present(activityViewController, animated: true, completion: nil)
     }
-
-    private func saveImageToGallery(image: UIImage) {
-        UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
-    }
-
-    @objc private func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-        if let error = error {
-            showAlertWith(title: "Save error", message: error.localizedDescription)
-        } else {
-            showAlertWith(title: "Saved!", message: "Your image has been saved to your photos.")
-        }
-    }
-
-    private func showAlertWith(title: String, message: String) {
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: .default))
-        present(alertController, animated: true, completion: nil)
-    }
-
 
     @objc func exitBarButtonItemTapped() {
         navigationController?.popViewController(animated: true)
@@ -125,24 +100,17 @@ class PhotoInfoViewController: UIViewController {
             photoImageView.heightAnchor.constraint(equalToConstant: 375),
             photoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             photoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        
         ])
     }
     
     private func setupNavigationBar() {
         navigationItem.leftBarButtonItem = exitBarButtonItem
         navigationItem.rightBarButtonItem = actionBarButtonItem
-        
-       // navigationController?.hidesBarsOnSwipe = true
-
-
     }
     
-   private func configure(with urlString: String) {
+    private func configure(with urlString: String) {
         if let url = URL(string: urlString) {
             photoImageView.sd_setImage(with: url, placeholderImage: UIImage(named: "placeholder"), options: .highPriority, completed: nil)
         }
     }
-
-    
 }
